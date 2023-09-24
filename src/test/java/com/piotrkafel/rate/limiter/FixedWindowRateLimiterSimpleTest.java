2de77324eaee6cce119c66a38d2b1648bb1621bc
pkafel <3944981+pkafel@users.noreply.github.com>
2023-09-24 23:08:59 +0200

    Add JUnit and first failing test

---
+++ src/test/java/com/piotrkafel/rate/limiter/FixedWindowRateLimiterSimpleTest.java
@@ -0,0 +1,13 @@
+package com.piotrkafel.rate.limiter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FixedWindowRateLimiterSimpleTest {
+
+    @Test
+    public void test() {
+        fail();
+    }
+}
